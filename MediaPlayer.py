751f4d993b961dc46b493f7e21a868b146577818
sandrorvr <sandrorvr@yahoo.com.br>
2023-03-05 12:59:44 -0300

    update

--- MediaPlayer.py
+++ MediaPlayer.py
@@ -2,6 +2,7 @@ from DataSpeak import DataSpeak
 from gtts import gTTS
 from playsound import playsound
 from os.path import exists
+from time import sleep
 
 from abc import ABC, abstractmethod
 
@@ -16,14 +17,15 @@ class MediaPlayer:
     def playerMP3(self, name_file):
         playsound(f'./file_mp3/{name_file}.mp3')
     
-    def loadSpeak(self, id_ask, data_ask, tp):
-        self.saveMP3(f'ask_{id_ask}', data_ask[tp])
+    def loadSpeak(self, id_ask, data_ask):
+        self.saveMP3(f'ask_{id_ask}', data_ask['ask'])
+        self.saveMP3(f'answer_{id_ask}', data_ask['answer'])
     
     def speak(self, tp, data):
         data_ask, id_ask = data
         print(data_ask[tp])
         if not exists(f'./file_mp3/ask_{id_ask}.mp3'):
-            self.loadSpeak(id_ask, data_ask, tp)
+            self.loadSpeak(id_ask, data_ask)
         
         self.playerMP3(f'{tp}_{id_ask}')
 
@@ -45,9 +47,15 @@ class MediaPlayer:
         while True:
             data_ask, id_ask = self.data.get_random_ask()
             state = self.control('repeat', (data_ask, id_ask))
-            if state == 'exit':
+            if state == 'answer':
+                self.speak('answer', (data_ask, id_ask))
+                sleep(3)
+            elif state == 'exit':
                 print('BY\n')
                 break
+            else:
+                self.speak('answer', (data_ask, id_ask))
+                sleep(3)
 
 
 if __name__ == "__main__":
